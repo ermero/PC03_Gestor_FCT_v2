35a09dc7cf78ae8060059dc585359c87e905fef3
ermero <ermero@floridauniversitaria.es>
2016-10-27 22:16:19 +0200

    primera version

---
+++ src/FCTBundle/Entity/Empresa.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace FCTBundle\Entity;
+
+/**
+ * Empresa
+ */
+class Empresa
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     */
+    private $direccion;
+
+    /**
+     * @var string
+     */
+    private $cp;
+
+    /**
+     * @var string
+     */
+    private $telefono1;
+
+    /**
+     * @var string
+     */
+    private $telefono2;
+
+    /**
+     * @var \DateTime
+     */
+    private $fechaCreacion;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Empresa
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set direccion
+     *
+     * @param string $direccion
+     *
+     * @return Empresa
+     */
+    public function setDireccion($direccion)
+    {
+        $this->direccion = $direccion;
+
+        return $this;
+    }
+
+    /**
+     * Get direccion
+     *
+     * @return string
+     */
+    public function getDireccion()
+    {
+        return $this->direccion;
+    }
+
+    /**
+     * Set cp
+     *
+     * @param string $cp
+     *
+     * @return Empresa
+     */
+    public function setCp($cp)
+    {
+        $this->cp = $cp;
+
+        return $this;
+    }
+
+    /**
+     * Get cp
+     *
+     * @return string
+     */
+    public function getCp()
+    {
+        return $this->cp;
+    }
+
+    /**
+     * Set telefono1
+     *
+     * @param string $telefono1
+     *
+     * @return Empresa
+     */
+    public function setTelefono1($telefono1)
+    {
+        $this->telefono1 = $telefono1;
+
+        return $this;
+    }
+
+    /**
+     * Get telefono1
+     *
+     * @return string
+     */
+    public function getTelefono1()
+    {
+        return $this->telefono1;
+    }
+
+    /**
+     * Set telefono2
+     *
+     * @param string $telefono2
+     *
+     * @return Empresa
+     */
+    public function setTelefono2($telefono2)
+    {
+        $this->telefono2 = $telefono2;
+
+        return $this;
+    }
+
+    /**
+     * Get telefono2
+     *
+     * @return string
+     */
+    public function getTelefono2()
+    {
+        return $this->telefono2;
+    }
+
+    /**
+     * Set fechaCreacion
+     *
+     * @param \DateTime $fechaCreacion
+     *
+     * @return Empresa
+     */
+    public function setFechaCreacion($fechaCreacion)
+    {
+        $this->fechaCreacion = $fechaCreacion;
+
+        return $this;
+    }
+
+    /**
+     * Get fechaCreacion
+     *
+     * @return \DateTime
+     */
+    public function getFechaCreacion()
+    {
+        return $this->fechaCreacion;
+    }
+}
+
---
+++ src/FCTBundle/Form/EmpresaType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace FCTBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+
+
+class EmpresaType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('direccion')
+            ->add('cp')
+            ->add('telefono1')
+            ->add('telefono2')
+            ->add('fechaCreacion', DateType::class)
+            ->add('Guardar',SubmitType::class)
+            ->add('Borrar',ResetType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FCTBundle\Entity\Empresa'
+        ));
+    }
+}
